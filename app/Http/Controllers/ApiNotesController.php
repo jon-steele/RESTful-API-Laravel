3ed7ad1805b8a309db6226341d41ba147731465f
Jon Steele <jonwhsteele@gmail.com>
2023-06-25 15:54:03 -0700

    Implements seeder and REST api for note

---
+++ app/Http/Controllers/ApiNotesController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Note;
+
+class ApiNotesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Note::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return Note::create($request->only(['title', 'body']));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Note::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $note = Note::findOrFail($id);
+        $note->update($request->only(['title', 'body']));
+        return $note;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Note::findOrFail($id)->delete();
+    }
+}
